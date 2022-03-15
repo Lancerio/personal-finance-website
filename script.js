8b6511ebba6f928b8214ffcc990e3b9601aa2006
lan <comiarln@gmail.com>
2022-03-15 21:02:55 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const quote = document.getElementById('quote-text');
+const author = document.getElementById('quote-author');
+const API_URL = 'https://type.fit/api/quotes';
+
+async function getQuote() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    displayQuote(data);
+
+}
+
+getQuote();
+
+function displayQuote(data) {
+    const randomQuote = data[Math.floor(Math.random() * data.length)];
+
+    quote.innerHTML = randomQuote.text;
+    author.innerHTML = `- ${randomQuote.author}`;
+
+    console.log(randomQuote.text);
+    console.log(randomQuote.author);
+}
